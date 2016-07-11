3a12479a3c9b9edc2fade7f4d41c83adf54f0fee
Vegard Øye <vegard_oye@hotmail.com>
2016-07-11 16:25:29 +0200

    tests

--- filter.js
+++ filter.js
@@ -1,17 +1,19 @@
+/* global angular */
+
 // Code defining custom module consisting of a filter
 // The module needs to be included as dependency for using the filter, titlecase
 angular.module('CustomFilterModule', [])
   .filter('titlecase', function () {
     return function (input) {
       return input.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      });
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+      })
     }
-  });
+  })
 
 // Angular App on this page
 // Included CustomFilterModule as dependency
 angular.module('DiftongApp', ['CustomFilterModule'])
   .controller('DiftongCtrl', ['$scope', function ($scope) {
-    $scope.name = '';
+    $scope.name = ''
   }])
---
+++ test/test.js
@@ -0,0 +1,9 @@
+/* global describe, it */
+describe('filter.js', function () {
+  describe('Example', function () {
+    it('should verify that a string is a string', function () {
+      var str = ''
+      str.should.be.a('string')
+    })
+  })
+})
